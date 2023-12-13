e157010eeded6111c9044a09d9aa627caf1a1919
Geovanny Campoverde <102754486+gcampoverde-altus@users.noreply.github.com>
2023-12-13 18:49:24 -0500

    Create RunBuild.groovy

---
+++ infra/ci/RunBuild.groovy
@@ -0,0 +1,41 @@
+@Library('pipeline-library-jenkins') _
+pipeline{
+
+    agent {
+        node {
+            label 'terraform1.2.5-ci'
+        }
+    }
+
+    environment {
+        app_dir="${WORKSPACE}/app-dummy-service-1"
+    }
+
+    parameters {
+          string defaultValue: 'develop', description: 'Select the desired branch and please make sure that the branch name matches with git branch', name: 'branch'
+
+    }
+
+
+    stages {
+
+        stage('Check out source code')
+        {
+            steps{
+                    script
+                    {
+
+                        withCredentials([usernamePassword(credentialsId: 'GITHUB_APP_PROVISION',
+                                          usernameVariable: 'GITHUB_APP',
+                                          passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
+
+                             sh """
+                                    git clone -b $branch https://"\$GITHUB_ACCESS_TOKEN":x-oauth-basic@github.com/gcampoverde-altus/app-dummy-service-1.git
+                                    cd $env.app_dir
+                                """
+                        }
+                    }
+                }
+        }
+    }
+}
